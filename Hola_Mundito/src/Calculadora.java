d42758d4f339694242baf768643604e7e472f040
Octavio Lucardi Fierro <octaviolf2016@gmail.com>
2022-10-04 01:02:00 -0300

    Actualizo Hola Mundo

--- Hola_Mundito/src/Calculadora.java
+++ Hola_Mundito/src/Calculadora.java
@@ -1,56 +1,20 @@
-import java.util.Scanner;
-
 public class Calculadora {
 
     Calculadora()
     {}
-    public int sumar() {
-        int numero1, numero2, resultado = 0;
-        Scanner s = new Scanner( System.in );
-
-        System.out.println( "Introduce dos numeros para sumar: ");
-        numero1 = s.nextInt();
-        numero2 = s.nextInt();
-        resultado = numero1 + numero2;
-
-        System.out.println( "La suma de " + numero1 + " y " + numero2 + " es: " + resultado );
-        return resultado;
+    public int sumar(int numero1, int numero2) {
+        return  numero1 + numero2;
     }
 
-    public int restar() {
-        int numero1, numero2, resultado = 0;
-        Scanner s = new Scanner( System.in );
-
-        System.out.println( "Introduce dos numeros para restar: ");
-        numero1 = s.nextInt();
-        numero2 = s.nextInt();
-        resultado = numero1 - numero2;
-
-        System.out.println( "La resta de " + numero1 + " y " + numero2 + " es: " + resultado );
-        return resultado;
+    public int restar(int numero1, int numero2) {
+        return numero1 - numero2;
     }
-    public int multiplicacion() {
-        int numero1, numero2, resultado = 0;
-        Scanner s = new Scanner( System.in );
+    public int multiplicacion(int numero1, int numero2) {
 
-        System.out.println( "Introduce dos numeros para multiplicar: ");
-        numero1 = s.nextInt();
-        numero2 = s.nextInt();
-        resultado = numero1 * numero2;
-
-        System.out.println( "La multiplicacion de " + numero1 + " y " + numero2 + " es: " + resultado );
-        return resultado;
+        return numero1*numero2;
     }
-    public float Division() {
-        float numero1, numero2, resultado = 0;
-        Scanner s = new Scanner( System.in );
+    public float Division(int numero1, int numero2) {
 
-        System.out.println( "Introduce dos numeros para dividir: ");
-        numero1 = s.nextInt();
-        numero2 = s.nextInt();
-        resultado = numero1 / numero2;
-
-        System.out.println( "La Division de " + numero1 + " y " + numero2 + " es: " + resultado );
-        return resultado;
+        return numero1/numero2;
     }
 }
--- Hola_Mundito/src/Main.java
+++ Hola_Mundito/src/Main.java
@@ -2,19 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         System.out.println("Hello world!");
-
-
         Calculadora calcular = new Calculadora();
-
-        calcular.sumar();
-        calcular.restar();
-        calcular.multiplicacion();
-        calcular.Division();
-
-
         }
     }
-
