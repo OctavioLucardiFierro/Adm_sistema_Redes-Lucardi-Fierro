06101610af2f371bf153dc7d90eb676f93d8446e
Octavio Lucardi Fierro <octaviolf2016@gmail.com>
2022-11-17 01:58:36 -0300

    Test unitarios api

--- Spring Boot/demo/demo/src/test/java/com/example/demo/CalculadoraOctavioLucardiFierroApplicationTests.java
+++ Spring Boot/demo/demo/src/test/java/com/example/demo/CalculadoraOctavioLucardiFierroApplicationTests.java
@@ -24,29 +24,56 @@ class CalculadoraOctavioLucardiFierroApplicationTests {
 	@Test
 	public void Multiplicacion(){
 		Assertions.assertEquals(Calcu.Multiplicar(3,5),15);
-		Assertions.assertNotEquals(Calcu.Multiplicar(21,76),0);
-		Assertions.assertNotEquals(Calcu.Multiplicar((-6),76),90);
+	}
+	@Test
+	public void Multiplicacion2(){
 		Assertions.assertEquals(Calcu.Multiplicar((-9),(-9)),81);
 	}
-
+	@Test
+	public void Multiplicacion3(){
+		Assertions.assertNotEquals(Calcu.Multiplicar(21,76),0);
+	}
+	@Test
+	public void Multiplicacion4(){
+		Assertions.assertNotEquals(Calcu.Multiplicar((-6),76),90);
+	}
 	@Test
 	public void Suma(){
 		Assertions.assertEquals(Calcu.SUMA(2,8),10);
+	}
+	@Test
+	public void Suma3(){
 		Assertions.assertNotEquals(Calcu.SUMA(21,2),122);
+	}
+	@Test
+	public void Suma2(){
 		Assertions.assertEquals(Calcu.SUMA((-9),(-9)),(-18));
 	}
-
 	@Test
 	public void Resta(){
 		Assertions.assertEquals(Calcu.RESTAR(90,55),35);
-		Assertions.assertNotEquals(Calcu.RESTAR(100,12),112);
-		Assertions.assertEquals(Calcu.RESTAR((-9),(-9)),0);
 	}
 
 	@Test
+	public void Resta2(){
+		Assertions.assertNotEquals(Calcu.RESTAR(100,12),112);
+	}
+
+	@Test
+	public void Resta3(){
+		Assertions.assertEquals(Calcu.RESTAR((-9),(-9)),0);
+	}
+	@Test
 	public void Divicion(){
 		Assertions.assertEquals(Calcu.Dividir(90,45),2.0);
+	}
+	@Test
+	public void Divicion2(){
 		Assertions.assertThrows(ArithmeticException.class,() -> {Calcu.Dividir(12000, 0);}); //Excepcion
+	}
+
+	@Test
+	public void Divicion3(){
 		Assertions.assertNotEquals(Calcu.RESTAR(3,4),0);
 	}
 
@@ -63,5 +90,31 @@ class CalculadoraOctavioLucardiFierroApplicationTests {
 	}
 
 
+	@Test
+	public void Cuadratica(){
+		double result1[] = {-1 , -1};
+
+		Assertions.assertArrayEquals(Calcu.FuncionCuadratica(1,2,1),result1,0);
+	}
+
+
+	@Test
+	public void Cuadratica2(){
+		double result2[] = { 1 , -5};
+
+		Assertions.assertArrayEquals(Calcu.FuncionCuadratica(1,4,(-5)),result2,0);
+	}
+
+	@Test
+	public void Cuadratica3(){
+		//Excepciones Matematicas
+		Assertions.assertThrows(ArithmeticException.class,() -> {Calcu.FuncionCuadratica(0,998,234);});
+	}
+
+
+	@Test
+	public void Cuadratica4(){
+		Assertions.assertThrows(ArithmeticException.class,() -> {Calcu.FuncionCuadratica(0,1,1);});
+	}
 
 }
